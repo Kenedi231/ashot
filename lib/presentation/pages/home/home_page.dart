9becfdb5457fbd678b01be62c0d4b77eea79ac3c
Alex Mubarakshin <muban96@mail.ru>
2020-05-18 19:23:24 +0300

    ✨ Add sign out

--- lib/presentation/pages/home/home_page.dart
+++ lib/presentation/pages/home/home_page.dart
@@ -1,11 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../../application/auth/auth_bloc.dart';
+import '../../routes/router.gr.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Главная страница'),
+    return MultiBlocListener(
+      listeners: [
+        BlocListener<AuthBloc, AuthState>(
+          listener: (context, state) {
+            state.maybeMap(
+              unauthenticated: (_) =>
+                  Router.navigator.pushReplacementNamed(Router.signInPage),
+              orElse: () {},
+            );
+          },
+        ),
+      ],
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Ашот'),
+          leading: IconButton(
+            icon: Icon(Icons.exit_to_app),
+            onPressed: () {
+              context.bloc<AuthBloc>().add(const AuthEvent.signedOut());
+            },
+          ),
+        ),
+        body: const Text('Главная страница'),
       ),
     );
   }
